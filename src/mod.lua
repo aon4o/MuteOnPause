6ac3c33ef7dc0e83b94d3dfc895719f17450b550
aon4o <a.o.naidenov@gmail.com>
2025-03-26 01:29:03 +0200

    removed debug lines

--- src/mod.lua
+++ src/mod.lua
@@ -10,14 +10,11 @@ local storedTrack = nil
 
 function mod:run()
     local isPaused = game:IsPaused()
-    local isHUDVisible = game:GetHUD():IsVisible()
 
     if isPaused and not wasPaused then
         storedTrack = music:GetCurrentMusicID()
-        print("Paused - Visible:", isHUDVisible)
         music:Pause()
     elseif not isPaused and wasPaused then
-        print("Unpaused - Visible:", isHUDVisible)
         if storedTrack and storedTrack ~= Music.MUSIC_NULL then
             music:Resume()
         end
